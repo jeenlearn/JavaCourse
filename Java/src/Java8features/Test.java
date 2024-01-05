8391616991859e8af9e2f523914cca1f350e8a80
jeenlearn <jeenaprem@gmail.com>
2024-01-05 12:24:21 +0100

    Added class

--- Java/src/Java8features/Test.java
+++ Java/src/Java8features/Test.java
@@ -1,4 +1,7 @@
-public static void main(String[]args){
+
+public class Test {
+    public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("Test2");
-        }
+    }
+}
